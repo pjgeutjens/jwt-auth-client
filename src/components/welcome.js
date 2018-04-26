e47a63b59870d978c3ebd3dbd46335a9d46b7f62
Pieter Jan Geutjens <pieter.geutjens@leapconsulting.be>
2018-04-26 11:07:52 +0200

    Auth middleware

---
+++ src/components/welcome.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Welcome extends Component {
+  render() {
+    return <div>welcome to our slice of paradise</div>;
+  }
+}
+
+export default Welcome;
